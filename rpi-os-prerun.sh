22c27c936147888e2b76c43993d788b1965c2c7f
Austin <auschick.work@gmail.com>
2023-08-16 00:54:45 -0700

    update tty

--- rpi-os-prerun.sh
+++ rpi-os-prerun.sh
@@ -12,7 +12,7 @@ touch /boot/ssh
 touch "$HOME/.xinitrc"
 
 cat >"$HOME/.bash_profile" <<EOF
-if [ -z \$DISPLAY ] && [ \$(tty) = /dev/tty3 ]
+if [ -z \$DISPLAY ] && [ \$(tty) = /dev/tty1 ]
 then
   startx >/dev/null 2>&1
 fi
